7cd9f0f54418b30811de1c950dacf4608b72c1f7
Hey4855 <tanicka.zgutova@gmail.com>
2025-02-09 17:56:56 +0100

    Co-authored-by: Tomas-Zgut <Tomas-Zgut@users.noreply.github.com>

--- app_hehe_yes/lib/main.dart
+++ app_hehe_yes/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Demonb',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -106,7 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'You have pushed the button this many times bro:',
             ),
             Text(
               '$_counter',
